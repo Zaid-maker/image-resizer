216ac4d8e50c148f042386cebf697dee2d02b6f3
Zaid-maker <pzhafeez@gmail.com>
2023-02-17 19:42:14 +0500

    add comments

--- preload.js
+++ preload.js
@@ -3,20 +3,24 @@ const path = require("path");
 const { contextBridge, ipcRenderer } = require("electron");
 const Toastify = require("toastify-js");
 
+/* Exposing the `os` module to the main world. */
 contextBridge.exposeInMainWorld("os", {
   homedir: () => os.homedir(),
 });
 
+/* Exposing the `path` module to the main world. */
 contextBridge.exposeInMainWorld("path", {
   join: (...args) => path.join(...args),
 });
 
+/* Exposing the ipcRenderer to the main world. */
 contextBridge.exposeInMainWorld("ipcRenderer", {
   send: (channel, data) => ipcRenderer.send(channel, data),
   on: (channel, func) =>
     ipcRenderer.on(channel, (event, ...args) => func(...args)),
 });
 
+/* Exposing the Toastify module to the main world. */
 contextBridge.exposeInMainWorld("Toastify", {
   toast: (options) => Toastify(options).showToast(),
 });
